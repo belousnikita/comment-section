53ad4c581f7f4851e7b366a58dd9c2eed04a06a0
Nikita Belous <belousnikita@gmail.com>
2019-03-12 21:24:22 +0200

    Class names changed, created comments line

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,82 +1,75 @@
 import React from "react";
-import data from "./data/services.json"
+import data from "./data/services.json";
 import ServiceTable from "./ServicesTable.jsx";
 
 const namebox = (name, position) => (
-  <div className="dialogue_header_name">
-    <p className="name">{name}</p>
-    <p className="position">{position}</p>
+  <div className="header_namebox">
+    <p className="header_namebox_name">{name}</p>
+    <p className="header_namebox_position">{position}</p>
   </div>
 );
 
-const avatar = (img) =>(
-  <div className="dialogue_header_image-cropper">
-    <img
-      className="dialogue_header_avatar"
-      src={img}
-      alt="Avatar"
-    />
+const avatar = img => (
+  <div className="header_image-cropper">
+    <img className="header_avatar" src={img} alt="Avatar" />
   </div>
 );
 
-const table = (
-  <div className="table_container">
-    <p className="table_header">Услуг</p>
-    <hr />
-    <table>
-      <tr className="first">
-        <td>Ручное бронирование</td>
-        <td className="table_number">11</td>
-      </tr>
-      <tr>
-        <td>Пакетные туры</td>
-        <td className="table_number">3</td>
-      </tr>
-      <tr className="last">
-        <td>Отели</td>
-        <td className="table_number">1</td>
-      </tr>
-    </table>
-    <div className="table_result">
-      <hr className="last_line" />
-      <span className="table_result_header">Всего</span>
-      <p className="table_result_number">15</p>
+const yellowBox = description => (
+  <div className="header_box">
+    <p className="header_box_text">{description}</p>
+  </div>
+);
+const commentsLine = (
+  <div className="comments_line">
+    <div className="comments_selector">
+      <div>Последние отзывы</div>
+      <div className="comments_link">Все отзывы</div>
+    </div>
+    <div className="comments_likes">
+      <img
+        className="comments_likes_heart"
+        src={require("./img/heart.png")}
+        alt="Heart"
+        id="heart"
+      />
+      <label for="heart">131</label>
+      <img
+        className="comments_likes_comment"
+        src={require("./img/comment.png")}
+        alt="Comment"
+        id="comment"
+      />
+      <label for="comment">14</label>
     </div>
   </div>
 );
 
-const yellowBox =(description) =>  (
-  <div className="dialogue_header_box">
-    <p className="dialogue_header_box_text">
-     {description}
-    </p>
-  </div>
-);
-
 export default class Page extends React.Component {
-    constructor(props) {
-      super(props);
-        this.name = props.name;
-        this.position = props.position;
-        this.description = props.description;
-        this.img = props.avatar;
-        this.state = {data: null};
-    }
-    componentDidMount() {
-      this.setState({data});
-    }
+  constructor(props) {
+    super(props);
+    this.name = props.name;
+    this.position = props.position;
+    this.description = props.description;
+    this.img = props.avatar;
+    this.state = { data: null };
+  }
+  componentDidMount() {
+    this.setState({ data });
+  }
   render() {
-    const {name, position, description, img} = this;
-    const {data} = this.state;
+    const { name, position, description, img } = this;
+    const { data } = this.state;
     return (
       <div className="container">
         <div className="dialogue">
-          <div className="dialogue_header">
+          <div className="header">
             {namebox(name, position)}
             {avatar(img)}
             {yellowBox(description)}
           </div>
-          {data && <ServiceTable data={data}></ServiceTable>}
+          {data && <ServiceTable data={data} />}
+          {commentsLine}
         </div>
       </div>
     );

ce615bff846d9dbebac4b921b14da7f0d62740cd
Nikita Belous <belousnikita@gmail.com>
2019-03-12 17:28:20 +0200

    React component created

--- src/index.js
+++ src/index.js
@@ -1,69 +1,17 @@
 import React from "react";
+import Page from "./Page";
 import ReactDom from "react-dom";
 import "./css/style.css";
 const root = document.getElementById("root");
 
-const name = (
-  <div className="dialogue_header_name">
-    <p className="name"> Вероника Ростова </p>
-    <p className="position"> Менедржер по продажам </p>
-  </div>
+ReactDom.render(
+  <Page
+    name={"Вероника Ростова"}
+    position={"Менеджер по продажам"}
+    description={
+      "Подберу для вас самые лучшие предложения. Мои услуги абсолютно бесплатны."
+    }
+    avatar={require("./img/avatar.png")}
+  />,
+  root
 );
-
-
-const avatar = (
-  <div className="dialogue_header_image-cropper">
-    <img className="dialogue_header_avatar"
-      src={require("./img/avatar.png")}
-      alt="Avatar"
-    />
-  </div>
-);
-
-const table = (
-  <div className="table_container">
-  <table>
-    <tr class="table_header">
-      <th/>
-      <th>Услуг</th>
-    </tr>
-    <tr>
-      <td>Ручное бронирование</td>
-      <td>11</td>
-    </tr>
-    <tr>
-      <td>Пакетные туры</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>Отели</td>
-      <td>1</td>
-    </tr>
-    <tr>
-      <th>Всего</th>
-      <th>15</th>
-    </tr>
-  </table>
-  </div>
-);
-const yellowBox = (
-  <div className="dialogue_header_box">
-    <p className="dialogue_header_box_text">Подберу для вас самые лучшие предложения. Мои услуги абсолютно бесплатны.</p>
-  </div>
-
-);
-
-const dom = (
-  <div className="container">
-    <div className="dialogue">
-      <div className="dialogue_header">
-        {name}
-        {avatar}
-        {yellowBox}
-      </div>
-      {table}
-    </div>
-  </div>
-);
-
-ReactDom.render(dom, root);

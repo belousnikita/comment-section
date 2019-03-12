7cd2ff3e2b970d469955d2139ba182532e815799
Nikita Belous <belousnikita@gmail.com>
2019-03-12 11:12:19 +0200

    Name and box added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import './css/style.css';
-const root = document.getElementById('root');
+import React from "react";
+import ReactDom from "react-dom";
+import "./css/style.css";
+const root = document.getElementById("root");
 
-const dom = <div className="container">
+const name = (
+  <div className="dialogue_header_name">
+    <p className="name">Вероника Ростова</p>
+    <p className="position">Менедржер по продажам</p>
+  </div>
+);
+
+const yellowBox = (
+  <div className="dialogue_header_box">
+    <p className="dialogue_header_box_text">Подберу для вас самые лучшие предложения. Мои услуги абсолютно бесплатны.</p>
+  </div>
+);
+const dom = (
+  <div className="container">
     <div className="dialogue">
-        <div className="dialogue_header">
-        <div className="dialogue_header_image-cropper"><img src={require("./img/avatar.png")} alt="Avatar"/> </div>
+      <div className="dialogue_header">
+        {name}
+        {yellowBox}
+        <div className="dialogue_header_image-cropper">
+          <img
+            className="dialogue_header_avatar"
+            src={require("./img/avatar.png")}
+            alt="Avatar"
+          />
         </div>
+      </div>
     </div>
-</div>;
+  </div>
+);
 
 ReactDom.render(dom, root);

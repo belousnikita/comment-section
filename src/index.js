2eca2f21fd19581f8eecf5089acc63fabde32f75
Nikita Belous <belousnikita@gmail.com>
2019-03-12 14:48:40 +0200

    Table created, avatar position fixed

--- src/index.js
+++ src/index.js
@@ -5,30 +5,63 @@ const root = document.getElementById("root");
 
 const name = (
   <div className="dialogue_header_name">
-    <p className="name">Вероника Ростова</p>
-    <p className="position">Менедржер по продажам</p>
+    <p className="name"> Вероника Ростова </p>
+    <p className="position"> Менедржер по продажам </p>
   </div>
 );
 
+
+const avatar = (
+  <div className="dialogue_header_image-cropper">
+    <img className="dialogue_header_avatar"
+      src={require("./img/avatar.png")}
+      alt="Avatar"
+    />
+  </div>
+);
+
+const table = (
+  <div className="table_container">
+  <table>
+    <tr class="table_header">
+      <th/>
+      <th>Услуг</th>
+    </tr>
+    <tr>
+      <td>Ручное бронирование</td>
+      <td>11</td>
+    </tr>
+    <tr>
+      <td>Пакетные туры</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>Отели</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <th>Всего</th>
+      <th>15</th>
+    </tr>
+  </table>
+  </div>
+);
 const yellowBox = (
   <div className="dialogue_header_box">
     <p className="dialogue_header_box_text">Подберу для вас самые лучшие предложения. Мои услуги абсолютно бесплатны.</p>
   </div>
+
 );
+
 const dom = (
   <div className="container">
     <div className="dialogue">
       <div className="dialogue_header">
         {name}
+        {avatar}
         {yellowBox}
-        <div className="dialogue_header_image-cropper">
-          <img
-            className="dialogue_header_avatar"
-            src={require("./img/avatar.png")}
-            alt="Avatar"
-          />
-        </div>
       </div>
+      {table}
     </div>
   </div>
 );

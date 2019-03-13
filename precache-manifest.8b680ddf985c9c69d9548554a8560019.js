9d5efe87eec35cb8d50d720007ec97f4301f32b9
Nikita Belous <belousnikita@gmail.com>
2019-03-13 16:56:11 +0200

    Updates

---
+++ precache-manifest.8b680ddf985c9c69d9548554a8560019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "802b5d90728644bd45058b1ea07d48ab",
+    "url": "/chern0us/202190311n/static/media/avatar.802b5d90.png"
+  },
+  {
+    "revision": "7ec0f25fc3d53cf289e0",
+    "url": "/chern0us/202190311n/static/js/runtime~main.96829157.js"
+  },
+  {
+    "revision": "1df8bbd50a8a5b98ccb1",
+    "url": "/chern0us/202190311n/static/js/main.eaa3be42.chunk.js"
+  },
+  {
+    "revision": "54e54121d295d689e780",
+    "url": "/chern0us/202190311n/static/js/2.987c8ddc.chunk.js"
+  },
+  {
+    "revision": "1df8bbd50a8a5b98ccb1",
+    "url": "/chern0us/202190311n/static/css/main.3a893d5a.chunk.css"
+  },
+  {
+    "revision": "be4fe9a1730bec51f926d7b1af63ab0d",
+    "url": "/chern0us/202190311n/index.html"
+  }
+];
